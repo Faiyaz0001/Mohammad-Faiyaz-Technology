fddf2084d3ae6837c597939d45ae8318bdbff43d
Faiyaz0001 <156143128+Faiyaz0001@users.noreply.github.com>
2024-01-10 13:34:48 -0800

    Add files via upload

---
+++ IntermediateJS.js
@@ -0,0 +1,218 @@
+//RANDOM NUMBER
+//syntsx:-Math.random()
+
+// console.log("Faiyaz");
+// var n =Math.random();
+// n=n * 6;
+// n=Math.floor(n)+1;
+// console.log(n);
+
+// prompt("What is your Name");
+
+// var loveScore=Math.random() *100;
+// loveScore=Math.floor(loveScore)+1;
+// console.log(loveScore);
+// alert("Hou Much Your Mind Work");
+
+// alert("Your Mind Work Only "+loveScore +"%");// prompt("What is your Name");
+
+// prompt("What is your Name");
+// prompt("What is their Name");
+
+// var loveScore=Math.random() *100;
+// loveScore=Math.floor(loveScore)+1;
+// console.log(loveScore);
+
+// if (loveScore>70){
+//     alert("Your love Score "+loveScore +"%" +"Your loves each other td Loves gd");
+// } else{
+//     alert("Your love Score "+loveScore +"%");
+// }
+
+// prompt("Name of student")
+// var b= Math.random()*100;
+// b=Math.floor(b)+1;
+// console.log(b);
+
+// if (b>30){
+//     alert("Congratulation!")
+//     alert("you passed first division with " +b +"% in the 12th classes");
+// }else {
+//     alert("you fail");
+// }
+
+
+// prompt("What is your Name");
+// prompt("What is their Name");
+
+// var loveScore=Math.random() *100;
+// loveScore=Math.floor(loveScore)+1;
+// console.log(loveScore);
+
+// if (loveScore>70){
+//     alert("Your love Score "+loveScore +"%" +"Your loves each other SF Loves FS");
+// } 
+
+// if (loveScore>30 && loveScore <=70){
+//     alert("your love score is "+loveScore+ "%");
+
+// }
+// if (loveScore<=30){
+//     alert("your love score is "+loveScore+ "%"+" you got together like oil and water")
+// }
+
+// function isleap(year){
+//     if (year % 4===0){
+//         if (year % 100===0){
+//             if (year % 400===0){
+//                 return "Leap YEAR";
+                
+//             }else{
+//                 return "Not leap year";
+//             }
+//         }else{
+//             return "Not leap year";
+//         }
+//     }else{
+//         return "Not leap year";
+//     }
+    
+// }
+
+// var b=isleap(2000);
+// alert(b);
+
+//ARRAYS JS
+
+// var guestList=["FAIYAZ","TABREZ","SAHIR","HIMANSHU"];
+// // console.log(guestList);
+// // console.log(guestList.length);
+// // console.log(guestList[0]);
+// // console.log(guestList[3]);
+
+// var guestName=prompt("What is your Name");
+// if (guestList.includes(guestName)){
+//     alert("Welcome to My Array.")
+
+// } else{
+//     alert("Sorry , May be Next Time.")
+// }
+
+// var output=[];
+v//ar count=1;
+
+// input.push(4);
+// input.push(5);
+// console.log(input);
+
+// function fizzBuzz(){
+//     output.push(count);
+//     count++;
+//     console.log(output);
+// }
+
+// var names=["FAIYAZ","TABREZ","SAHIR","HIMANSHU"];
+// alert(name);
+
+// function whoPaying(names){
+//     var numofPeope=names.Lenght;
+//     var randomPersonPosition=Math.floor(Math.random()* numofPeope);
+//     var randomPerson=[randomPersonPosition];
+//     return randomPerson + " is going to buy lunch today";
+// }
+
+//WHILE LOOP
+// var i =1;
+// while (i<2){
+//     console.log(i);
+//     i++;
+// }
+// alert(i);
+
+// for (let i=0;i<10;i++){
+//     console.log(i);
+
+// }
+
+// console.log("Faiyaz");
+// alert("faiyaz");
+
+
+
+
+
+// function fibonacciGenerator(n){
+
+
+//     var output=[];
+//     if (n===1){
+//     output=[0];
+
+// }
+
+// else if(n===2){
+//     output=[0,1];
+   
+
+
+// }
+// else{
+//     output[0,1]; 
+//     for (i=2;i<n;i++){
+//     output.push(output[output.length-2]+output[output.length-1]);
+// }
+// }
+// return output;
+
+// }
+// output= fibonacciGenerator(1);
+// console.log(output);
+
+
+
+Document.querySelector("h1").innerHTML="Good By!";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
